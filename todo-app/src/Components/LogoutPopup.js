22d0e0f4e92d9aa682a8ca8e37ccfa91997eccf3
Bilgehan Akcan <b.akcan013@gmail.com>
2021-07-20 13:26:47 +0300

    Logout Popup, comment is added

--- todo-app/src/Components/LogoutPopup.js
+++ todo-app/src/Components/LogoutPopup.js
@@ -2,6 +2,8 @@ import React from "react";
 import {Link} from "react-router-dom"
 import {Cookies} from "react-cookie";
 
+//Popup that asks whether you are sure to log out
+
 function LogoutPopup(props) {
     const cookies = new Cookies();
     function insidePopup() {
